4a84aa82747c4eddc7323cdbc9545fd5ee4ab3a4
MTummino <48964541+MTummino@users.noreply.github.com>
2019-05-07 14:39:02 -0400

    Update Common.java

--- JSONParseDemo/src/com/MTummino/PlacesJSON/Common.java
+++ JSONParseDemo/src/com/MTummino/PlacesJSON/Common.java
@@ -2,7 +2,7 @@ package com.MTummino.PlacesJSON;
 
 public class Common
 {
-	final static String APIKEY = "AIzaSyB61rRsv01vmiGigb6i591zloQw5ZNvmoc";
+	final static String APIKEY = "XXXXXXXXXXXXXXXX";
 	final static String BASEURL =  "https://maps.googleapis.com/maps/api/place/nearbysearch/";
 	final static String URLFORMAT = "json";
 	final static String URLRADIUS = "5000";
